8330c045b7073db223c8d2d1d913dec57f3d93a6
pundrich <pundrich@gmail.com>
2019-06-06 18:11:16 -0400

    Clean old version

--- ML/LTSM/fastai_master/tests/test_utils_fastai.py
+++
@@ -1,6 +0,0 @@
-import pytest, fastai
-from fastai.gen_doc.doctest import this_tests
-
-def test_has_version():
-    this_tests('na')
-    assert fastai.__version__
--- ML/test/first_code.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Jun  4 16:27:02 2019
-
-@author: gabrielpundrich
-"""
-
-banana  = 5
